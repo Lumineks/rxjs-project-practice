8d8ae20720b6d4a03e9f0d1f270fee1237a459ab
Michail_N <mihasik323@gmail.com>
2021-11-10 17:08:38 +0200

    add button click observable

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { filter, fromEvent, fromEventPattern, map } from "rxjs";
 
 function App() {
+  const btnRef = useRef<HTMLButtonElement>(null);
+  console.log("here");
+
+  useEffect(() => {
+    const observable = fromEvent(btnRef.current!, "click");
+    console.log(observable);
+
+    const subscribtion = observable.subscribe({next: (event) => {
+      console.log("clicked", event);
+    }});
+
+    return () => subscribtion.unsubscribe();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn" type="button" ref={btnRef}>
+        Button
+      </button>
     </div>
   );
 }
